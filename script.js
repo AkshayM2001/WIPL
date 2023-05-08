7cd217a634d9e8531a4f5ec0bf61a55d4bed67c6
AkshayM2001 <123633241+AkshayM2001@users.noreply.github.com>
2023-05-08 22:25:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let wi =document.getElementById("wi");
+let ham =document.getElementById("ham");
+let vo =document.getElementById("vo");
+let kl =document.getElementById("kl");
+
+window.addEventListener('scroll',() => {
+    let value = window.scrollY;
+
+    wi.style.left = value * -0.5 + 'px';
+    ham.style.left = value * -0.1 + 'px';
+    vo.style.left = value * 0.1 + 'px';
+    kl.style.left = value * 0.5 + 'px';
+    }
+);
